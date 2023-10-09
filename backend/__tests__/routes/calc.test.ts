7639b9cd1ffeb74385842f09712f396eb6d7cb50
Kolade Ige <kolade.ige@gmail.com>
2023-10-09 23:39:47 +0100

    extended test timeout period to accomodate dbserver response.

--- backend/__tests__/routes/calc.test.ts
+++ backend/__tests__/routes/calc.test.ts
@@ -57,7 +57,7 @@ beforeEach(async () => {
       answer: 3,
     }
   );
-});
+}, 20000);
 
 afterAll(async () => {
   await Calc.deleteMany();
@@ -73,18 +73,18 @@ describe("API routes", () => {
   it("~/api/calc/history should return all calculations history items.", async () => {
     const items = await Calc.find();
     request(app).get("/api/calc/history").expect(200, items);
-  });
+  }, 10000);
 
   it("~/api/calc/history/clear should clear calculation history", async () => {
     request(app).get("/api/calc/history/clear").expect(200);
 
     request(app).get("/api/calc/history").expect(200, []);
-  });
+  }, 10000);
 
   it("~/api/calc/run/add return the correct sum and save the history", async () => {
     const operation = {
       param1: 8,
-      param2: 7
+      param2: 7,
     };
     const res = await request(app).post("/api/calc/run/add").send(operation);
 
@@ -95,7 +95,7 @@ describe("API routes", () => {
     expect(answer).toEqual("15");
 
     expect((await Calc.find()).length).toEqual(11);
-  });
+  }, 10000);
 
   it("~/api/calc/run/sub return the correct difference and save the history", async () => {
     const operation = {
@@ -111,7 +111,7 @@ describe("API routes", () => {
     expect(answer).toEqual("-10");
 
     expect((await Calc.find()).length).toEqual(11);
-  });
+  }, 10000);
 
   it("~/api/calc/run/mul return the correct multiplication and save the history", async () => {
     const operation = {
@@ -127,7 +127,7 @@ describe("API routes", () => {
     expect(answer).toEqual("800");
 
     expect((await Calc.find()).length).toEqual(11);
-  });
+  }, 10000);
 
   it("~/api/calc/run/div return the correct division and save the history", async () => {
     const operation = {
@@ -157,5 +157,5 @@ describe("API routes", () => {
     expect(answer2).toEqual("0.1");
 
     expect((await Calc.find()).length).toEqual(12);
-  });
+  }, 10000);
 });
